678f30273c1915cba42c513745d5e8e3c656f8c7
Ben Romine <elementnumber35@gmail.com>
2019-03-21 14:49:25 -0400

    Optimized for https

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :task_tracker_spa, TaskTrackerSpaWeb.Endpoint,
   root: ".",
   version: Application.spec(:phoenix_distiller, :vsn),
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "tasks3.element35.org", port: 80],
+  url: [host: "tasks3.element35.org", scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
